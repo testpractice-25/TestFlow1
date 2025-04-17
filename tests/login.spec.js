a76dcfda151c286cf6f47e7d735cd773035f2b82
Neha <neha@Nehas-MacBook-Pro.local>
2025-04-17 16:32:07 +0530

    Initial commit - TestFlow1

---
+++ tests/login.spec.js
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+test( "Login test on Demo Site", async({page}) => {
+
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.fill('#username', 'student');
+    await page.fill('#password', 'Password123');
+    await page.click('#submit');
+
+    await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+
+});
