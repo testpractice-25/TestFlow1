b14a20974ab47e4c9dec723652109d367f977476
Neha <neha@Nehas-MacBook-Pro.local>
2025-04-21 01:55:16 +0530

    using correct webhook3

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ test( "Login test on Demo Site", async({page}) => {
 
     await page.goto('https://practicetestautomation.com/practice-test-login/');
     await page.fill('#username', 'student');
-    await page.fill('#password', 'Password123xyz');
+    await page.fill('#password', 'Password123xyzo');
     await page.click('#submit');
 
     await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
