19adbfc6ebb8117f4ebd41d0395da8dd2550d2de
Neha <neha@Nehas-MacBook-Pro.local>
2025-04-21 19:34:55 +0530

    hit commit from locale

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,7 +4,7 @@ test( "Login test on Demo Site", async({page}) => {
 
     await page.goto('https://practicetestautomation.com/practice-test-login/');
     await page.fill('#username', 'student');
-    await page.fill('#password', 'Password123x');
+    await page.fill('#password', 'Password123');
     await page.click('#submit');
 
     await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
